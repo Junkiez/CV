082be03923017eb6e550b5ec80bcee58427439da
Kaze <80313506+Junkiez@users.noreply.github.com>
2022-11-04 09:53:32 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,6 @@ function App() {
                     <Radar/>
                     <Technologies/>
                 </article>
-                <Separator title="Experience" />
             </main>
             <footer className="flex-row">
                 <Tessaract/>
